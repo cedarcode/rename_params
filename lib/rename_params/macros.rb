b1706e0e68822dc3b4685f168359d7bc49dd4bf5
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2016-11-11 22:10:43 -0300

    Enable filtering

--- lib/rename_params/macros.rb
+++ lib/rename_params/macros.rb
@@ -5,11 +5,21 @@ module RenameParams
     class_methods do
       def rename(*args)
         current_param = args.shift
-        before_action "rename_param_#{current_param}".to_sym
+        before_action_options = extract_callback_options(*args)
+        before_action "rename_param_#{current_param}".to_sym, before_action_options
 
         define_method("rename_param_#{current_param}") do
           Params.new(params).rename(current_param, *args)
         end
+      end
+
+      private
+
+      def extract_callback_options(args = {})
+        {
+          only: args.delete(:only),
+          except: args.delete(:except)
+        }.compact
       end
     end
   end
--- spec/macros_spec.rb
+++ spec/macros_spec.rb
@@ -19,6 +19,13 @@ describe RenameParams::Macros, type: :controller do
           expected = default_params.merge(login: 'aperson')
           expect(controller.params).to eq ActionController::Parameters.new(expected)
         end
+
+        context 'if param is not sent' do
+          it 'leaves params as they were' do
+            get :index
+            expect(controller.params).to eq ActionController::Parameters.new(default_params)
+          end
+        end
       end
 
       context 'when converting values' do
@@ -36,6 +43,13 @@ describe RenameParams::Macros, type: :controller do
             expected = default_params.merge(role: ['admin'])
             expect(controller.params).to eq ActionController::Parameters.new(expected)
           end
+
+          context 'if param is not sent' do
+            it 'leaves params as they were' do
+              get :index
+              expect(controller.params).to eq ActionController::Parameters.new(default_params)
+            end
+          end
         end
 
         context 'and using a Proc converter' do
@@ -50,6 +64,83 @@ describe RenameParams::Macros, type: :controller do
           it 'renames amount_due to amount_due_in_cents and converts value' do
             get :index, params: { amount_due: 100 }
             expected = default_params.merge(amount_due_in_cents: 10000)
+            expect(controller.params).to eq ActionController::Parameters.new(expected)
+          end
+
+          context 'if param is not sent' do
+            it 'leaves params as they were' do
+              get :index
+              expect(controller.params).to eq ActionController::Parameters.new(default_params)
+            end
+          end
+        end
+      end
+    end
+
+    context 'when filtering' do
+      context 'using only' do
+        controller(ActionController::Base) do
+          rename :username, to: :login, only: :show
+
+          def index
+            head :ok
+          end
+
+          def show
+            head :ok
+          end
+        end
+
+        describe 'show' do
+          before { routes.draw { get 'show' => 'anonymous#show' } }
+
+          it 'renames username to login' do
+            get :show, params: { username: 'aperson' }
+            expected = { controller: 'anonymous', action: 'show', login: 'aperson' }
+            expect(controller.params).to eq ActionController::Parameters.new(expected)
+          end
+        end
+
+        describe 'index' do
+          before { routes.draw { get 'index' => 'anonymous#index' } }
+
+          it 'keeps username param' do
+            get :index, params: { username: 'aperson' }
+            expected = { controller: 'anonymous', action: 'index', username: 'aperson' }
+            expect(controller.params).to eq ActionController::Parameters.new(expected)
+          end
+        end
+      end
+
+      context 'using except' do
+        controller(ActionController::Base) do
+          rename :username, to: :login, except: :show
+
+          def index
+            head :ok
+          end
+
+          def show
+            head :ok
+          end
+        end
+
+        describe 'show' do
+          before { routes.draw { get 'show' => 'anonymous#show' } }
+
+          it 'keeps username param' do
+            get :show, params: { username: 'aperson' }
+            expected = { controller: 'anonymous', action: 'show', username: 'aperson' }
+            expect(controller.params).to eq ActionController::Parameters.new(expected)
+          end
+        end
+
+        describe 'index' do
+          before { routes.draw { get 'index' => 'anonymous#index' } }
+
+          it 'renames username to login' do
+            get :index, params: { username: 'aperson' }
+            expected = { controller: 'anonymous', action: 'index', login: 'aperson' }
             expect(controller.params).to eq ActionController::Parameters.new(expected)
           end
         end

5d90bcdd1e520d25957257b06ebeca06d01786b0
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2016-11-11 21:38:04 -0300

    DRY macros_spec

--- spec/macros_spec.rb
+++ spec/macros_spec.rb
@@ -1,63 +1,57 @@
 describe RenameParams::Macros, type: :controller do
   describe '.rename' do
-    context 'when not converting values' do
-      controller(ActionController::Base) do
-        rename :username, to: :login
 
-        def index
-          head :ok
-        end
-      end
+    context 'when not filtering' do
+      let(:default_params) { { controller: 'anonymous', action: 'index' } }
+      before { routes.draw { get index: 'anonymous#index' } }
 
-      before do
-        routes.draw { get 'index' => 'anonymous#index' }
-        get :index, params: { username: 'aperson' }
-      end
-
-      it 'renames username to login' do
-        expected = { controller: 'anonymous', action: 'index', login: 'aperson' }
-        expect(controller.params).to eq ActionController::Parameters.new(expected)
-      end
-    end
-
-    context 'when converting values' do
-      context 'and using enum converter' do
+      context 'when just renaming params' do
         controller(ActionController::Base) do
-          rename :admin, to: :role, convert: { true: ['admin'], false: [] }
+          rename :username, to: :login
 
           def index
             head :ok
           end
         end
 
-        before do
-          routes.draw { get 'index' => 'anonymous#index' }
-          get :index, params: { admin: 'true' }
-        end
-
-        it 'renames admin to role and converts value' do
-          expected = { controller: 'anonymous', action: 'index', role: ['admin'] }
+        it 'renames username to login' do
+          get :index, params: { username: 'aperson' }
+          expected = default_params.merge(login: 'aperson')
           expect(controller.params).to eq ActionController::Parameters.new(expected)
         end
       end
 
-      context 'and using a Proc converter' do
-        controller(ActionController::Base) do
-          rename :amount_due, to: :amount_due_in_cents, convert: ->(value) { value.to_i * 100 }
+      context 'when converting values' do
+        context 'and using enum converter' do
+          controller(ActionController::Base) do
+            rename :admin, to: :role, convert: { true: ['admin'], false: [] }
 
-          def index
-            head :ok
+            def index
+              head :ok
+            end
+          end
+
+          it 'renames admin to role and converts value' do
+            get :index, params: { admin: 'true' }
+            expected = default_params.merge(role: ['admin'])
+            expect(controller.params).to eq ActionController::Parameters.new(expected)
           end
         end
 
-        before do
-          routes.draw { get 'index' => 'anonymous#index' }
-          get :index, params: { amount_due: 100 }
-        end
+        context 'and using a Proc converter' do
+          controller(ActionController::Base) do
+            rename :amount_due, to: :amount_due_in_cents, convert: ->(value) { value.to_i * 100 }
 
-        it 'renames admin to role and converts value' do
-          expected = { controller: 'anonymous', action: 'index', amount_due_in_cents: 10000 }
-          expect(controller.params).to eq ActionController::Parameters.new(expected)
+            def index
+              head :ok
+            end
+          end
+
+          it 'renames amount_due to amount_due_in_cents and converts value' do
+            get :index, params: { amount_due: 100 }
+            expected = default_params.merge(amount_due_in_cents: 10000)
+            expect(controller.params).to eq ActionController::Parameters.new(expected)
+          end
         end
       end
     end

62e79fe541af9e06d822fb09fc6d6305032bc997
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2017-06-22 16:57:50 -0300

    Remove rails3.2 appraisals

--- Appraisals
+++ Appraisals
@@ -1,8 +1,3 @@
-appraise 'rails-3-2' do
-  gem 'rails', '~>3.2.0'
-  gem 'test-unit', '~> 3.0'
-end
-
 appraise 'rails-4-2' do
   gem 'rails', '~>4.2.0'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,6 @@ ENV['DATABASE_URL'] = 'sqlite3://localhost/tmp/rename_params_test'
 require 'bundler/setup'
 require 'rails'
 case Rails.version
-  when '3.2.22.5'
-    require 'support/apps/rails3_2'
   when '4.2.7.1'
     require 'support/apps/rails4_2'
   when '5.0.0.1'
